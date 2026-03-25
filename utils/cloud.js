9f0922e5e51054354c157d1de1952c532a8a55f6
porall <625513044@qq.com>
2026-03-25 12:18:24 +0800

    fix: 优化cloud.js代码

--- utils/cloud.js
+++ utils/cloud.js
@@ -1,64 +1,45 @@
 /**
  * 云函数封装
+ * 注意：小程序中 wx.cloud 已在 app.js 中初始化
  */
-const cloud = wx.cloud
-
-const cloudApi = {
-  /**
-   * 获取广场卡组列表
-   */
-  getSquareList: function(data = {}) {
-    return cloud.callFunction({
+var cloudApi = {
+  getSquareList: function(data) {
+    return wx.cloud.callFunction({
       name: 'getSquareList',
-      data: data
+      data: data || {}
     })
   },
 
-  /**
-   * 获取卡组详情
-   */
   getDeckDetail: function(deckId) {
-    return cloud.callFunction({
+    return wx.cloud.callFunction({
       name: 'getDeckDetail',
       data: { deckId: deckId }
     })
   },
 
-  /**
-   * 发布卡组
-   */
   publishDeck: function(data) {
-    return cloud.callFunction({
+    return wx.cloud.callFunction({
       name: 'publishDeck',
       data: data
     })
   },
 
-  /**
-   * 收藏/取消收藏
-   */
   likeDeck: function(deckId) {
-    return cloud.callFunction({
+    return wx.cloud.callFunction({
       name: 'likeDeck',
       data: { deckId: deckId }
     })
   },
 
-  /**
-   * 使用卡组（导入到本地）
-   */
   useDeck: function(deckId) {
-    return cloud.callFunction({
+    return wx.cloud.callFunction({
       name: 'useDeck',
       data: { deckId: deckId }
     })
   },
 
-  /**
-   * 添加评论
-   */
   addComment: function(deckId, content) {
-    return cloud.callFunction({
+    return wx.cloud.callFunction({
       name: 'addComment',
       data: { deckId: deckId, content: content }
     })
